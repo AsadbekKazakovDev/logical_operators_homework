93b76df75f42ac2e91eabbcf7246ff23965ff395
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-22 13:47:35 +0500

    18-masala yechimi

--- logic18.py
+++ logic18.py
@@ -5,4 +5,14 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    bir = a//10000
+    ikki = (a//1000)%10
+    uch = (a//100)%10
+    turt = (a//10)%10
+    besh = a%10
+    k = bir <ikki and ikki<uch and uch<turt and turt<besh
+    return k
+a = 12345
+print(main(a))
+a = 54321
+print(main(a))
