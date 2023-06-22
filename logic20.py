7b0eeff3f061bf74a0b14a74ee4d10c801c84788
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-22 22:42:14 +0500

    20-masala yechimi

--- logic20.py
+++ logic20.py
@@ -8,5 +8,18 @@ def main(n):
     Returns:
         bool: answer
     """
+    x1=n%10
+    n//=10
+    x2=n%10
+    n//=10
+    x3=n%10
+    n//=10
+    x4=n%10
+    n//=10
+    x5=n%10
 
-    return
+    return (n>=1000 and x1+x2+x3+x4+x5>2) or (n<1000 and x1+x2+x3+x4+x5>=2)
+n = 101000
+print(main(n))
+n = 1101
+print(main(n))
