803069bd956adacb876b0fb6b69e921f5494588c
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-22 13:45:01 +0500

    17-masala yechimi

--- logic17.py
+++ logic17.py
@@ -5,5 +5,17 @@ def main(a):
         a(int): parameter a
     Returns:
         bool: answer
+    
     """
-    return
+    bir = a//10000
+    ikki = (a//1000)%10
+    uch = (a//100)%10
+    turt = (a//10)%10
+    besh = a%10
+    k = bir>ikki and ikki>uch and uch>turt and turt>besh
+   
+    return k 
+a = 12345
+print(main(a))
+a = 75421
+print(main(a))
